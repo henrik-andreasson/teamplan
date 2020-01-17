cd644029a7ec48be8d5dc851a59dc836b0ef8d5e
Henrik Andreasson <henrik.andreasson@cgi.com>
2020-01-17 20:00:51 +0100

    remove qoutes from token

--- utils/rest-work-list.sh
+++ utils/rest-work-list.sh
@@ -5,7 +5,7 @@
 user_name="admin"
 pass_word="foo123"
 
-token=$(http --auth "$user_name:$pass_word" POST http://localhost:5000/api/tokens | jq ".token")
+token=$(http --auth "$user_name:$pass_word" POST http://localhost:5000/api/tokens | jq ".token" | sed 's/\"//g')
 
 
 http --verbose http://localhost:5000/api/worklist \
