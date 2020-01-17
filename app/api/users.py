2d347e70e492b22d38ef5c53c10e87c5e6f1ea8d
Henrik Andreasson <henrik.andreasson@cgi.com>
2020-01-17 19:58:08 +0100

    reguire token auth for user api

--- app/api/users.py
+++ app/api/users.py
@@ -1,22 +1,21 @@
 from app.api import bp
-from flask import jsonify
+from flask import jsonify, current_app
 from app.models import User
 from flask import url_for
 from app import db
 from app.api.errors import bad_request
 from flask import request
-from flask import g, abort
 from app.api.auth import token_auth
+from flask import g, abort
 
 @bp.route('/users', methods=['POST'])
+@token_auth.login_required
 def create_user():
     data = request.get_json() or {}
-    if 'username' not in data or 'email' not in data or 'password' not in data:
-        return bad_request('must include username, email and password fields')
+    if 'username' not in data or 'password' not in data:
+        return bad_request('must include username andpassword fields')
     if User.query.filter_by(username=data['username']).first():
         return bad_request('please use a different username')
-    if User.query.filter_by(email=data['email']).first():
-        return bad_request('please use a different email address')
     user = User()
     user.from_dict(data, new_user=True)
     db.session.add(user)
